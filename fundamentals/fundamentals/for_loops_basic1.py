a601717a80408f00ca9be45a429f2edc95a18a17
davidpierce24 <pie2ce@gmail.com>
2022-05-02 21:27:24 -0400

    for loops

---
+++ fundamentals/fundamentals/for_loops_basic1.py
@@ -0,0 +1,39 @@
+# 1 Basic
+for basic in range(0,150):
+    print(basic)
+
+# 2 Multiples of Five
+for mult5 in range(0, 1001, 5):
+    print(mult5)
+
+for mult5 in range(0,1001):
+    if(mult5%5 == 0):
+        print(mult5)
+
+# 3 Count, the Dojo Way
+for mult in range(1, 101):
+    if(mult%10 == 0):
+        print("Coding Dojo")
+    elif (mult%5 == 0):
+        print("Coding")
+    else:
+        print(mult)
+
+# 4 Whoa, that sucker's huge
+    sum = 0
+for count in range(1, 500000):
+    if(count%2 == 1):
+        sum += count
+print(sum)
+
+# 5 Countdown by fours
+for countdown in range(2018, 0, -4):
+    print(countdown)
+
+# 6 Flexible Counter
+lowNum = 2
+highNum = 9
+mult = 3
+for counter in range(lowNum, highNum+1):
+    if(counter%mult == 0):
+        print(counter)
