6343823711777f6d30ea55260bfebf5346a72abf
davidpierce24 <pie2ce@gmail.com>
2022-05-03 11:50:07 -0400

    oop example

---
+++ fundamentals/oop/oop_example.py
@@ -0,0 +1,8 @@
+employees = []
+
+employees.append({'first_name': 'Alex', 'last_name': 'Smith', 'salary':87000})
+employees.append({'first_name': 'Bradley', 'last_name': 'Jones', 'salary':62000})
+employees.append({'first_name': 'Charlie', 'last_name': 'Adams', 'salary':89000})
+
+for employee in employees:
+    print(f"First name: {employee['first_name']}")
