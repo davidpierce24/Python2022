d37234adb7a60a8858a22827698579b99ff6b303
davidpierce24 <pie2ce@gmail.com>
2022-05-03 11:56:18 -0400

    updates

--- fundamentals/fundamentals/functions_intermediate_1.py
+++ fundamentals/fundamentals/functions_intermediate_1.py
@@ -30,8 +30,9 @@ print(z)
 
 # 2 Iterate through a list of dictionaries
 def iterateDictionary(some_list):
-    for x in range(0, len(some_list)):
-            print (some_list[x])
+    for x in some_list:
+            print (f"first_name - {x['first_name']}, last_name - {x['last_name']}")
+            # print(x)
 
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -42,10 +43,10 @@ students = [
 iterateDictionary(students) 
 # should output: (it's okay if each key-value pair ends up on 2 separate lines;
 # bonus to get them to appear exactly as below!)
-first_name - Michael, last_name - Jordan
-first_name - John, last_name - Rosales
-first_name - Mark, last_name - Guillen
-first_name - KB, last_name - Tonel
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
 
 
 
