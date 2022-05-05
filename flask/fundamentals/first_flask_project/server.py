95389f6f6cacde54f5f7ff6bcbc7b66fa63739b6
davidpierce24 <pie2ce@gmail.com>
2022-05-05 16:13:33 -0400

    updates

--- flask/fundamentals/first_flask_project/server.py
+++ flask/fundamentals/first_flask_project/server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello world :3'
+    return render_template("index.html")
 
 @app.route('/another_route')
 def another_route():
@@ -16,7 +16,26 @@ def test_data(route_data):
 
 @app.route('/multiply/<int:x>/<int:y>')
 def multiply_two_numbers(x, y):
-    return f"The result of {x} * {y} = {x*y}"
+    return render_template("multiply.html", x_temp = x, y_temp = y, result = x*y)
+
+@app.route('/multiplication_table/<int:x>/<int:y>')
+def multiplication_table(x,y):
+    results = []
+
+    new_row = ["X"]
+    for k in range(0, x+1):
+        new_row.append(k)
+    results.append(new_row)
+
+    for i in range(0, y+1):
+        new_row = [i]
+        for j in range(0, x+1):
+            new_row.append(i*j)
+        results.append(new_row)
+    print(results)
+    return render_template("multitable.html", x = x, y = y, results = results)
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
