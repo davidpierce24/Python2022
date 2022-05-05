1dd2c717dcad6ffd37577ba2db46e6a838af7fc1
davidpierce24 <pie2ce@gmail.com>
2022-05-05 17:43:22 -0400

    updates

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -9,6 +9,18 @@ def hello_world():
 def dojo():
     return "Dojo!"
 
+@app.route('/say/<word>')
+def say_word(word):
+    return f"Hi {word}!"
+
+@app.route('/repeat/<int:x>/<word>')
+def repeat(word, x):
+    # return word * x
+    list = ""
+    for item in range(0, x+1):
+        list += f"{word}\r\n"
+    return list
+
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
 
