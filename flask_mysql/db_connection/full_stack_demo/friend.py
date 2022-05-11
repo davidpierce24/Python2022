45dfc1a8c498251c41f2f29de2f94d7fe28893e7
davidpierce24 <pie2ce@gmail.com>
2022-05-11 17:57:56 -0400

    first full stack

---
+++ flask_mysql/db_connection/full_stack_demo/friend.py
@@ -0,0 +1,51 @@
+# import the function that will return an instance of a connection
+from winreg import QueryInfoKey
+from mysqlconnection import connectToMySQL
+
+
+# model the class after the friend table from our database
+class Friend:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.occupation = data['occupation']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+
+    # Now we use class methods to query our database
+    @classmethod
+    def get_all(cls):
+        # creating a variable that represents the actual SQL query
+        query = "SELECT * FROM friends;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL('friends_schema').query_db(query)
+        # Create an empty list to append our instances of friends
+        friends = []
+        # Iterate over the db results and create instances of friends with cls.
+        for friend in results:
+            friends.append( cls(friend) )
+        return friends
+
+
+
+    # Method to create a friend
+    @classmethod
+    def create_friend(cls, data):
+        query = "INSERT INTO friends (first_name, last_name, occupation) VALUES (%(first_name)s, %(last_name)s, %(occupation)s);"
+        return connectToMySQL('friends_schema').query_db(query, data)
+
+
+    # Method to update a friend
+    @classmethod
+    def update_friend(cls,data):
+        query = "UPDATE friends SET first_name = %(first_name)s, last_name = %(last_name)s, occupation = %(occupation)s WHERE id = %(id)s;"
+        connectToMySQL('friends_schema').query_db(query, data)
+
+
+    # Method to delete a friend
+    @classmethod
+    def delete_friend(cls, data):
+        query = "DELETE FROM friends WHERE id = %(id)s"
+        connectToMySQL('friends_schema').query_db(query, data)
---
+++ flask_mysql/db_connection/full_stack_demo/server.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, redirect
+
+
+# import the class from friend.py
+from friend import Friend
+
+
+app = Flask(__name__)
+
+# Route to render all friends
+@app.route("/")
+def index():
+    # call the get all classmethod to get all friends
+    friends = Friend.get_all()
+    print(friends)
+    return render_template("index.html", all_friends = friends)
+
+# Route to create a friend
+@app.route('/create', methods=["POST"])
+def create_friend():
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'occupation': request.form['occupation']
+    }
+
+    Friend.create_friend(data)
+
+    return redirect("/")
+
+
+# Route to render update form
+@app.route('/renderUpdate/<int:id>')
+def render_update(id):
+
+
+
+    return render_template('update.html', id =  id)
+
+
+
+# Route to update a friend
+@app.route('/update', methods=["POST"])
+def update():
+
+    data = {
+        'id': request.form['id'],
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'occupation': request.form['occupation']
+    }
+
+    Friend.update_friend(data)
+    
+    return redirect('/')
+
+
+# Route to delete a friend
+@app.route("/delete/<int:id>")
+def delete(id):
+
+    data = {
+        'id': id
+    }
+
+    Friend.delete_friend(data)
+    return redirect('/')
+
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
