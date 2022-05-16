ca42ec634b6b5f233e0a8526993816cec7619872
davidpierce24 <pie2ce@gmail.com>
2022-05-16 15:32:43 -0400

    updates

---
+++ flask_mysql/validation/login/flask_app/controllers/users.py
@@ -0,0 +1,21 @@
+from crypt import methods
+from flask_app import app
+
+from flask import render_template, redirect, request, session
+
+
+@app.route('/')
+def index():
+
+    return render_template("index.html")
+
+
+@app.route('/users/register', methods=['POST'])
+def register_user():
+
+    # validate user
+
+
+    # then if valid, add to DB
+
+    return('/')
