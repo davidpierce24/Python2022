45982a09894e353f710c04516c10c88063a7fad3
davidpierce24 <pie2ce@gmail.com>
2022-05-16 19:21:57 -0400

    updates

--- flask_mysql/validation/login/flask_app/controllers/users.py
+++ flask_mysql/validation/login/flask_app/controllers/users.py
@@ -1,21 +1,40 @@
 from flask_app import app
 from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
 from flask import render_template, redirect, request, session
+from flask_app.models.user import User
 
 
+# Route to render home page
 @app.route('/')
 def index():
 
     return render_template("index.html")
 
 
+# Route to process user registration
 @app.route('/users/register', methods=['POST'])
 def register_user():
 
     # validate user
+    if not User.validate_user(request.form):
+        return redirect('/')
 
 
     # then if valid, add to DB
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
 
-    return('/')
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email'],
+        'password': pw_hash
+    }
+
+    user_id = User.add_user(data)
+
+    session['user_id'] = user_id
+
+    return redirect('/')
--- flask_mysql/validation/login/flask_app/models/user.py
+++ flask_mysql/validation/login/flask_app/models/user.py
@@ -1,3 +1,4 @@
+from pydoc import classname
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 import re
@@ -14,12 +15,77 @@ class User:
         self.updated_at = data['updated_at']
 
 
+    # Validation #################################################
     @staticmethod
     def validate_user(data):
 
         is_valid = True
 
-        # username must be a valid length
-        if len(data['username']) < 3 or len(data['username']) > 20:
+        # checks to see if first name is only letters with at least 2 characters
+        if data['first_name'].isalpha() == False or len(data['first_name']) < 2:
+            flash("Please ensure first name only consists of letters and is longer than 1 letter")
             is_valid = False
-            flash()
+
+
+        # checks to see if first name is only letters with at least 2 characters
+        if data['last_name'].isalpha() == False or len(data['last_name']) < 2:
+            flash("Please ensure last name only consists of letters and is longer than 1 letter")
+            is_valid = False
+
+
+        # checks to see if email is valid based on regular expression
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        
+        if not EMAIL_REGEX.match(data['email']): 
+            flash("Invalid email address!")
+            is_valid = False
+
+
+        # checks to see if email already exists in database
+        email_input = { "email" : data["email"] }
+        user_in_db = User.get_by_email(email_input)
+        
+        if not user_in_db:
+            flash("Email already exists")
+            is_valid = False
+
+
+        # checks to see if email was submitted
+        if len(data['email']) < 1:
+            flash("Please add a valid email")
+            is_valid = False
+
+
+        # checks to see if password is longer than 8 characters
+        if len(data['password']) < 8:
+            flash("Password must be 8 characters long")
+            is_valid = False
+
+        
+        # checks to see if password confirmation mathces
+        if data['password'] != data['confirm_password']:
+            flash("Passwords must match exactly")
+            is_valid = False
+
+    
+    # ###############################################################
+
+
+
+    # Method to pull a user based on an email
+    @classmethod
+    def get_by_email(cls, data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        results = connectToMySQL('login_schema').query_db(query, data)
+        if len(results) < 1:
+            return False
+        user = User(results[0])
+        return user
+
+
+    # Method to add a user to the database
+    @classmethod
+    def add_user(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"
+        results = connectToMySQL('login_schema').query_db(query, data)
+        return results
