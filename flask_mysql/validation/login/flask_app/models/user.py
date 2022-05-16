b2f19191f2445ea40d5effad37f398dc1c90f6f7
davidpierce24 <pie2ce@gmail.com>
2022-05-16 15:47:07 -0400

    updates

--- flask_mysql/validation/login/flask_app/models/user.py
+++ flask_mysql/validation/login/flask_app/models/user.py
@@ -1,2 +1,24 @@
+from curses import flash
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask import Flash
 
+
+class User:
+    def __init__(self, data):
+        self.id = data['id']
+        self.username = data['username']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+
+    @staticmethod
+    def validate_user(data):
+
+        is_valid = True
+
+        # username must be a valid length
+        if len(data['username']) < 3 or len(data['username']) > 20:
+            is_valid = False
+            flash()
