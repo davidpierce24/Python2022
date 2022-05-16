22cd4e43045d43c471dcc772be7df4240bcb8f5f
davidpierce24 <pie2ce@gmail.com>
2022-05-16 00:54:43 -0400

    updates

--- flask_mysql/crud/books/flask_app/controllers/authors.py
+++ flask_mysql/crud/books/flask_app/controllers/authors.py
@@ -3,3 +3,12 @@ from flask_app import app
 from flask import render_template, redirect, session, request
 
 from flask_app.models.author import Author
+
+
+
+# Route to render the main authors page
+@app.route('/')
+def show_authors():
+
+
+    return render_template("authors.html")
--- flask_mysql/crud/books/flask_app/controllers/books.py
+++ flask_mysql/crud/books/flask_app/controllers/books.py
@@ -3,3 +3,12 @@ from flask_app import app
 from flask import render_template, redirect, request, session
 
 from flask_app.models.book import Book
+
+
+
+# Route to render books main page
+@app.route('/books')
+def show_books():
+
+
+    return render_template("books.html")
