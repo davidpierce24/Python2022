f222c5a8f27902f4ea4c2d1c562ce37dc9e68049
davidpierce24 <pie2ce@gmail.com>
2022-05-17 15:02:01 -0400

    updates

--- flask_mysql/belt_review/recipes/flask_app/controllers/recipes.py
+++ flask_mysql/belt_review/recipes/flask_app/controllers/recipes.py
@@ -1,4 +1,4 @@
 from flask_app import app
 
 from flask import render_template, request, redirect, session, flash
-from flask_app.models.recipe import Recipe
+# from flask_app.models.recipe import Recipe
--- flask_mysql/belt_review/recipes/flask_app/controllers/users.py
+++ flask_mysql/belt_review/recipes/flask_app/controllers/users.py
@@ -1,6 +1,34 @@
+from crypt import methods
 from flask_app import app
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 
 from flask import render_template, redirect, request, session, flash
-from flask_app.models.user import User
+# from flask_app.models.user import User
+
+# Route to render the login and registration page
+@app.route('/')
+def home():
+
+    return render_template("login.html")
+
+
+# Route to process user registration
+@app.route('/register', methods=["POST"])
+def register():
+
+
+
+    return redirect('/')
+
+
+
+
+
+# Route to process user login
+@app.route('/login', methods=["POST"])
+def login():
+
+
+
+    return redirect('/')
