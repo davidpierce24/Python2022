3e19cd9c2be9179f36d38df4a0f6730fe372f457
davidpierce24 <pie2ce@gmail.com>
2022-05-18 02:03:39 -0400

    updates

--- flask_mysql/belt_review/recipes/flask_app/controllers/recipes.py
+++ flask_mysql/belt_review/recipes/flask_app/controllers/recipes.py
@@ -4,4 +4,25 @@ from flask import render_template, request, redirect, session, flash
 from flask_app.models.recipe import Recipe
 
 
-# Route to take user to edit page
+# Route to take user to edit recipe page
+@app.route('/edit/recipe/<int:id>')
+def to_recipe(id):
+
+    return render_template("edit_recipe.html", id = id)
+
+
+
+#  Route to edit recipe
+@app.route('/edit/recipe' methods=["POST"])
+def edit_recipe():
+
+    data = {
+        'id': request.form['id'],
+        'name': request.form['name'],
+        'description': request.form['description'],
+        'instructions': request.form['instructions'],
+        'created_at': request.form['created_at'],
+        'under_thirty': request.form['under_thirty']
+    }
+
+    Recipe.edit_recipe(data)
